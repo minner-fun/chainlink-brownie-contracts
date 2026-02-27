3c6c862374d9d879dab4c550aa16e0ed2337d7eb
qfm_ky <qifumin2017@163.com>
2026-02-27 14:28:59 +0800

    change createSubscription the subId creation logic

--- contracts/src/v0.8/vrf/dev/SubscriptionAPI.sol
+++ contracts/src/v0.8/vrf/dev/SubscriptionAPI.sol
@@ -347,8 +347,9 @@ abstract contract SubscriptionAPI is ConfirmedOwner, IERC677Receiver, IVRFSubscr
   function createSubscription() external override nonReentrant returns (uint256 subId) {
     // Generate a subscription id that is globally unique.
     uint64 currentSubNonce = s_currentSubNonce;
+    bytes32 prevHash = block.number > 0 ? blockhash(block.number - 1) : bytes32(0); 
     subId = uint256(
-      keccak256(abi.encodePacked(msg.sender, blockhash(block.number - 1), address(this), currentSubNonce))
+      keccak256(abi.encodePacked(msg.sender, address(this), currentSubNonce))
     );
     // Increment the subscription nonce counter.
     s_currentSubNonce = currentSubNonce + 1;
